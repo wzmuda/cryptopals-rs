f13d00a52b5d33b248ef6871e0bc260179c8b07d
Wojciech Zmuda <zmuda.w@gmail.com>
2023-01-26 21:43:54 +0100

    set1: chllenge3: add input string frequency analysis to dicover key
    Signed-off-by: Wojciech Zmuda <zmuda.w@gmail.com>

--- challenges/src/challenge3.rs
+++ challenges/src/challenge3.rs
@@ -1,22 +1,32 @@
 //! Cryptopals :: Set 1 :: Challenge 3
 //! The hex encoded string has been XOR'd against a single character.
 //! Find the key, decrypt the message.
-
+use std::collections::HashMap;
 use utils::ToVecExt;
 
 fn is_control_char(c: char) -> bool {
     (c as u8) < 32 // ascii values below space are non-printable
 }
 
-fn xored_hex_str_find_key(_input: &str) -> char {
-    'X'  // I guessed it; TODO key discovery
+fn xored_hex_str_find_key(input: &Vec<u8>) -> char {
+    // Build a hashmap with bytes' values as keys and their frequency in that vector as values
+    let mut m = HashMap::new();
+    for b in input {
+        *m.entry(b).or_insert(0) +=1;
+    }
 
+    // Assumption: ' ' (space) is the most frequent character in an English sentence
+    let (most_frequent_char, _) = m.iter().max_by_key(|(_, count)| *count).unwrap();
+
+    const SPACE_ASCII: u8 = 32;
+    // TODO why is most_frequent_char &&u8?!
+    (**most_frequent_char ^ SPACE_ASCII) as char
 }
 
 fn xored_hex_str_decrypt(input: &str) -> String {
     let v = input.to_vec().unwrap();
+    let key = xored_hex_str_find_key(&v);
     let mut out = String::new();
-    let key = xored_hex_str_find_key(input);
 
     for b in v.into_iter() {
         let c = (b ^ key as u8) as char;
